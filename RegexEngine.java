428f20f40d54fb62fcc486ff6019b0d0714fde43
loxeh <lochiecapy@gmail.com>
2026-03-17 08:20:02 +1030

    Set up basic input reading and read output

---
+++ RegexEngine.java
@@ -0,0 +1,20 @@
+// importing scanner for for higher level reading methods (as System.in only provides raw bytes)
+// buffering input, detecting newlines, splitting input into tokens (similar to scanf in C)
+import java.util.Scanner;
+
+public class RegexEngine {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); // Scanner wrapping System.in
+
+        String regex = scanner.nextLine(); // reads regex from first line of input and returns it as a string (similar to fgets)
+
+        System.out.println("ready"); // print that the scanner is ready
+
+        // read and evaluate input strings for any subsequent inputs 
+        while (scanner.hasNextLine()){
+            String input = scanner.nextLine();
+            System.out.println(input); 
+        }
+    }
+    
+}
---
+++ RegexEnginer_Text.java
@@ -0,0 +1,3 @@
+public class RegexEnginer_Text {
+    
+}
