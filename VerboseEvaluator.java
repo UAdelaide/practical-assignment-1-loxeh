e6f35c51ee1a07601b8755dc34d8d79da214022d
loxeh <lochiecapy@gmail.com>
2026-03-24 09:22:31 +1030

    Added in VerboseEvaluator.java
    - seperate class/file for the verbose evaluator to keep modular
    - outlined the basic format of what the output of transition table should look like
    
    no AI used here

---
+++ VerboseEvaluator.java
@@ -0,0 +1,14 @@
+// regex the user has enetered should print a transition trable for the ɛ-NFA generated followed by the word "ready"
+
+/* following the example given, we need:
+- a header row (epsilon a   b   c   other)
+- rows per state (state name using q)
+- epsilon transitions and character transitions for each state 
+- > for starting state
+- * for accepting state
+*/
+
+
+public class VerboseEvaluator {
+    
+}
